0ae0c8d6a6079f7faf0ca4fcc5972f43efc972fc
Razvan Cranganu <razvan.cranganu@wundermanthompson.com>
2021-12-09 10:13:13 +0000

    Sorting beers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [beers, setBeers] = useState([])
+
+  const baseUrl = 'https://api.punkapi.com/v2/beers/';
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((beers) => {
+        setBeers(beers)
+      })
+  }, [searchTerm])
+
+
+  const handleInput = (event) => {
+    const inputValue = event.target.value
+    setSearchTerm(inputValue)
+  }
+
+  return(
+    <div>
+      React App Beers
+      <form>
+        <input type="text" id="inputValue" value={searchTerm} onInput={handleInput} />
+      </form> 
+      {
+        beers
+        .filter(item => {
+          if (!searchTerm) return true
+          if (item.name.includes(searchTerm)) {
+            return true
+          }
+        })
+        .map(item => (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <span>{item.tagline}</span>
+            <img src={item.image_url} />
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
